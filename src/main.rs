7fba1597e23803b9a62262f704ef45561b0f3579
bikathi <bikathimartin@outlook.com>
2026-04-24 11:47:22 +0300

    Implement logic for demo-ing RwLock across threads

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,106 @@
+use std::time::{SystemTime, UNIX_EPOCH};
 use std::{
     collections::HashMap,
-    sync::{Arc, RwLock, mpsc},
+    sync::{
+        Arc, RwLock,
+        mpsc::{self},
+    },
+    thread::{self, Builder},
+    time::Duration,
 };
 
-use log::info;
+use log::{error, info};
+
+#[derive(Debug)]
+enum Operation {
+    GET,
+    SET,
+}
+
+impl Operation {
+    fn randomize_operation(min: i32, max: i32) -> Self {
+        let nanos = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_nanos();
+
+        // Use the modulo operator to stay within the range
+        // This replicates: Math.floor(Math.random() * (max - min + 1)) + min
+        let random = (nanos % (max - min + 1) as u128) as i32 + min;
+
+        match random {
+            1 => Self::GET,
+            2 => Self::SET,
+            _ => Self::randomize_operation(min, max),
+        }
+    }
+}
 
 fn main() {
     // init env_logger
     env_logger::init();
 
-    // this is the key data structure we will be editting over threads
+    // represents our in-mem DB
     let db: Arc<RwLock<HashMap<String, String>>> = Arc::new(RwLock::new(HashMap::new()));
 
     // this is the messaging channel
-    let (sender, receiver) = mpsc::channel::<&str>();
+    let (sender, receiver) = mpsc::channel::<String>();
+
+    // initialize the Monitor threads
+    for index in 1..=5 {
+        let db_clone = Arc::clone(&db);
+        let builder = Builder::new().name(format!("{}", index));
+        let csender = sender.clone();
+        match builder.spawn(move || {
+            loop {
+                let operation = Operation::randomize_operation(1, 2);
+                match operation {
+                    // for read operations
+                    Operation::GET => {
+                        let _rl = db_clone.read().unwrap(); // create a readlock
+                        csender
+                            .send(format!(
+                                "Thread {t} performing operation {o:?}",
+                                t = index,
+                                o = operation
+                            ))
+                            .unwrap();
+                        // sleep with the lock for a while
+                        thread::sleep(Duration::from_secs(3u64));
+                    }
+                    Operation::SET => {
+                        let _wl = db_clone.write().unwrap(); // create a write lock
+                        csender
+                            .send(format!(
+                                "Thread {t} performing operation {o:?}: (Sleeping)",
+                                t = index,
+                                o = operation
+                            ))
+                            .unwrap();
+                        // sleep with the lock for a while
+                        thread::sleep(Duration::from_secs(3u64));
+                    }
+                }
+
+                csender
+                    .send(format!(
+                        "Thread {t} completed operation {o:?}",
+                        t = index,
+                        o = operation
+                    ))
+                    .unwrap();
+            }
+        }) {
+            Ok(_) => (),
+            Err(e) => {
+                error!("Unable to create thread handle! Cause: {:?}", e);
+                break;
+            }
+        }
+    }
 
     // the monitoring logic
-    for received in receiver.iter() {
+    for received in receiver {
         info!("{received}")
     }
 }
