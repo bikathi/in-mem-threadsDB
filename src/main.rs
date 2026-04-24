95f0c0e128e2672d03a31652314005d29e9bf7b4
bikathi <bikathimartin@outlook.com>
2026-04-24 09:50:06 +0300

    Implement the MPSC receiver end

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock, mpsc},
+};
+
+use log::info;
+
 fn main() {
-    println!("Hello, world!");
+    // init env_logger
+    env_logger::init();
+
+    // this is the key data structure we will be editting over threads
+    let db: Arc<RwLock<HashMap<String, String>>> = Arc::new(RwLock::new(HashMap::new()));
+
+    // this is the messaging channel
+    let (sender, receiver) = mpsc::channel::<&str>();
+
+    // the monitoring logic
+    for received in receiver.iter() {
+        info!("{received}")
+    }
 }
